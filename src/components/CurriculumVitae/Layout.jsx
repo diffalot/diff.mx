806e8a8bca2068ff50b549e97746e71cec33a3ef
Alice Davis <alice@gigantic.computer>
2020-12-30 03:26:23 -0800

    reset to basically no styling except for the latex fonts :)

--- src/components/CurriculumVitae/Layout.jsx
+++ src/components/CurriculumVitae/Layout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import './fonts.css'
+import './Layout.css'
+
 const Layout = ({ children }) => {
   console.log({ children })
-  return <div className="sm:container mx-auto">{children}</div>
+  return <div className="cv sm:container mx-auto">{children}</div>
 }
 
 Layout.propTypes = {
