650a517b730d7dce1c53d823fcf4c4659d8dabaa
Alice Davis <alice@gigantic.computer>
2021-01-07 18:51:05 -0800

    happy with large and desktop views

--- src/components/CurriculumVitae/CV.jsx
+++ src/components/CurriculumVitae/CV.jsx
@@ -5,12 +5,16 @@ import './page.css'
 import './fonts.css'
 import './markdown.css'
 
-const Layout = ({ children }) => {
-  return <div className="cv px-5 print:px-0 py-16 print:py-0">{children}</div>
+const CV = ({ children }) => {
+  return (
+    <div className="cv m-auto xl:text-xl px-5 print:px-0 py-16 print:py-0">
+      <div className="xl:m-20">{children}</div>
+    </div>
+  )
 }
 
-Layout.propTypes = {
+CV.propTypes = {
   children: PropTypes.node.isRequired
 }
 
-export default Layout
+export default CV
--- src/components/CurriculumVitae/Section.jsx
+++ src/components/CurriculumVitae/Section.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types'
 import { MDXProvider } from '@mdx-js/react'
 
 const Section = ({ title, children }) => (
-  <section className="flex flex-col print:flex-row align-top mb-3">
-    <h1 className="print:w-1/4 font-bold uppercase mr-3 mb-2">{title}</h1>
-    <div className="print:w-3/4 pb-2">
+  <section className="flex flex-col md:flex-row print:flex-row align-top mb-3">
+    <h1 className="md:w-1/4 print:w-1/5 font-bold uppercase mr-3 mb-2">
+      {title}
+    </h1>
+    <div className="md:w-3/4 print:w-4/5 pb-2">
       <MDXProvider>{children}</MDXProvider>
     </div>
   </section>
