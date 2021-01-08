cc9f3636da9c6026b11dfe5deb762f27a7a26371
Alice Davis <alice@gigantic.computer>
2021-01-08 06:32:03 -0800

    pow is looking neat

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import Pow from '../images/pow.inline.svg'
+
 const Header = ({ siteTitle }) => (
   <div className="bg-green-300 dark:bg-indigo-600">
     <nav className="container m-auto p-4 xl:px-44 flex justify-between items-end">
@@ -9,6 +11,7 @@ const Header = ({ siteTitle }) => (
         {siteTitle}
       </Link>
       <Link to="/cv" className="font-bold uppercase">
+        <Pow />
         Hire Me
       </Link>
     </nav>
