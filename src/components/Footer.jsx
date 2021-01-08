68cf55876bdcde7fe2e7500402598f1778e4f763
Alice Davis <alice@gigantic.computer>
2021-01-08 11:39:54 -0800

    add footer with github link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,24 +7,26 @@ import CC from '../images/creativecommons-cc.inline.svg'
 import CCBy from '../images/creativecommons-by.inline.svg'
 
 const Footer = ({ githubURL }) => (
-  <div className="bg-green-300 dark:bg-indigo-600">
+  <div className="bg-green-300 dark:bg-indigo-600 text-right">
     <div className="sm:container mx-auto p-4 font-bold">
-      <p>
+      <p className=" align-bottom">
         <a
           rel="license"
           href="https://creativecommons.org/licenses/by/4.0/"
           title="Creative Commons Attribution 4.0 International license">
-          <CC className="inline h-9 -m-4" />
-          <CCBy className="inline h-9 -mr-2" />
-          2014-2021 Alice Davis
+          <CC className="inline h-3 -ml-6 -mr-12" />
+          <CCBy className="inline h-3 -mr-5" />
+          <span className="mt-3 text-sm">2014-2021 Alice Davis</span>
         </a>
       </p>
-      {githubURL ? (
+      {!githubURL ? (
         ''
       ) : (
-        <p className="pt-3">
-          Source code available at
-          <Link to={githubURL}>{githubURL}</Link>
+        <p className="pt-3 text-xs font-light">
+          {/* prettier-ignore */}
+          <Link target="_blank" to={githubURL}>
+            Source code available on GitHub
+          </Link>
         </p>
       )}
     </div>
@@ -36,7 +38,7 @@ Footer.propTypes = {
 }
 
 Footer.defaultProps = {
-  githubURL: false
+  githubURL: ''
 }
 
 export default Footer
