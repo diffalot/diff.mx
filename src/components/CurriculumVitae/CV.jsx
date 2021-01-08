ba15ef9cfe865a50d59f34a7ace4c7f579d44885
Alice Davis <alice@gigantic.computer>
2021-01-07 18:05:47 -0800

    happy with mobile portrait version

--- src/components/CurriculumVitae/CV.jsx
+++ src/components/CurriculumVitae/CV.jsx
@@ -6,7 +6,7 @@ import './fonts.css'
 import './markdown.css'
 
 const Layout = ({ children }) => {
-  return <div className="cv">{children}</div>
+  return <div className="cv px-5 print:px-0 py-16 print:py-0">{children}</div>
 }
 
 Layout.propTypes = {
