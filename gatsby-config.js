d6cf205b0c42aa384a08ab6c3f602d06c9db72b1
Alice Davis <alice@gigantic.computer>
2021-01-08 06:29:01 -0800

    add react svg modules

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,14 @@ module.exports = {
     },
     'gatsby-plugin-react-helmet',
     {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /\.inline\.svg$/
+        }
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `static`,
