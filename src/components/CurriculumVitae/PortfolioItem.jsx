44ebbb4c31855380ad7dee8299cf2d71fa2471e1
Alice Davis <alice@gigantic.computer>
2021-01-21 13:50:00 -0800

    minimal styling

--- src/components/CurriculumVitae/PortfolioItem.jsx
+++ src/components/CurriculumVitae/PortfolioItem.jsx
@@ -2,18 +2,44 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { MDXProvider } from '@mdx-js/react'
 
-const PortfolioItem = ({ name, status, toolchain, link, image, children }) => (
-  <section className="">
-    <p>{name}</p>
-    <p>{toolchain}</p>
-    {image ? <img src={image} /> : null}
-    <p>
-      <a target="_blank" href={link}>
-        {link}
-      </a>
-    </p>
-    {children}
-  </section>
-)
+const PortfolioItem = ({
+  name,
+  homepage,
+  status,
+  toolchain,
+  link,
+  image,
+  children
+}) => {
+  const nameOrLink = () => {
+    if (homepage) {
+      return (
+        <a target="_blank" href={homepage}>
+          {name}
+        </a>
+      )
+    } else {
+      return <span>{name}</span>
+    }
+  }
+
+  return (
+    <section className="pb-6">
+      <header className="italic">
+        {image ? <img src={image} /> : null}
+        <div className="flex justify-between">
+          <div>{nameOrLink()}</div>
+          <div>
+            <a target="_blank" href={link}>
+              {link}
+            </a>
+          </div>
+        </div>
+        <div className="pb-2">{toolchain}</div>
+      </header>
+      {children}
+    </section>
+  )
+}
 
 export default PortfolioItem
